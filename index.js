239b89450d3005830ab3286d211387ab6cc2f634
Sefa ÜN <sefa.un@tsoft.com.tr>
2022-03-21 17:25:49 +0300

    replica yapıldı

--- index.js
+++ index.js
@@ -1,38 +1,21 @@
 import Redis from "ioredis"
 
-const client = new Redis.Cluster([
-  {
-    port: 7000,
-    host: "localhost",
-  },
-  {
-    port: 7001,
-    host: "localhost",
-  },
-  {
-    port: 7002,
-    host: "localhost",
-  },
-  {
-    port: 7003,
-    host: "localhost",
-  },
-  {
-    port: 7004,
-    host: "localhost",
-  },
-  {
-    port: 7005,
-    host: "localhost",
-  },
-])
+const client = new Redis({port: 7002})
 
-client.on("connect", (data) => {
-  console.log(data, "bağlandı")
+client.on("connect", () => {
+  console.log("bağlandı")
 })
 
-client.set("sefa", "bar");
-
+//client.set("sefa3", "barw");
+setInterval(() => {
 client.get("sefa").then(data => {
   console.log(data)
 }).catch(err => console.log(err, "hata"))
+}, 500);
+// setInterval(() => {
+
+//   client.xread("BLOCK", 100, "COUNT", 100, "STREAMS", "abooo", "$").then(data => {
+//     console.log(data)
+//   }).catch(err => console.log(err, "\n-> read error"))
+
+// }, 500);
--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@
 https://www.dltlabs.com/blog/how-to-setup-configure-a-redis-cluster-easily-573120
 
 https://iamvishalkhare.medium.com/create-a-redis-cluster-faa89c5a6bb4
+
+https://github.com/marcel-dempers/docker-development-youtube-series/tree/master/storage/redis/clustering
